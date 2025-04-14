e335bd6212c6769052604e6463e2a52c256b9339
TalhaZaheer1 <tzaheer72@gmail.com>
2025-04-14 05:54:24 -0700

    changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const { verifyToken } = require("../middlewares/authMiddleware");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.get("/profile", verifyToken, authController.profile);
+router.get("/profile",  authController.profile);
 router.post("/logout", authController.logout);
 
 // NEW
